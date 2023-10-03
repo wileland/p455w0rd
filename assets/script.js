1b2564ddbf4ed96dc8137bea6cb4171b19b2e120
wileland <wileland7@gmail.com>
2023-10-03 07:13:42 -0500

    Updated Logic of functions (JS).

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,45 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
+    let passwordLength = parseInt(prompt('Enter password length (between 8 and 128):'));
 
-  passwordText.value = password;
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        passwordLength = parseInt(prompt('Invalid. Enter password length (between 8 and 128):'));
+    }
 
+    const isLowercase = confirm('Include lowercase letters?');
+    const isUppercase = confirm('Include uppercase letters?');
+    const isNumeric = confirm('Include numbers?');
+    const isSpecialChar = confirm('Include special characters?');
+
+    if (!isLowercase && !isUppercase && !isNumeric && !isSpecialChar) {
+        alert('At least one character type should be selected.');
+        return "";
+    }
+
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numericChars = '0123456789';
+    const specialChars = '!@#$%^&*()-_=+[]{}|;:,.<>?/';
+
+    let allChars = '';
+    if (isLowercase) allChars += lowercaseChars;
+    if (isUppercase) allChars += uppercaseChars;
+    if (isNumeric) allChars += numericChars;
+    if (isSpecialChar) allChars += specialChars;
+
+    let generatedPassword = '';
+    for (let i = 0; i < passwordLength; i++) {
+        generatedPassword += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    return generatedPassword;
 }
 
-// Add event listener to generate button
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
 generateBtn.addEventListener("click", writePassword);
